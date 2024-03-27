dcc85d861bdce31bf6c8ac52540a28133cf9a094
Arcane <106981249+ArcaneAw@users.noreply.github.com>
2024-03-27 09:30:30 -0700

    ModFly.lua

--- ModFly.lua
+++ ModFly.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/ArcaneAw/boombox/main/AC'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ArcaneAw/boombox/main/boombox.lua'))()
 
 function Fly()
 	function sandbox(var,func)
